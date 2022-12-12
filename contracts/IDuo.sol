9245461ae4685d3673155cd108a213981cdd4146
Tayfun Sur <tayfunsur@gmail.com>
2022-12-12 23:27:49 +0300

    Create interfaces for NFT and marketplace contracts

---
+++ contracts/IDuo.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+interface IDuo {
+    function balanceOf(address owner) external view returns (uint256);
+
+    /// @dev tokenOfOwnerByIndex returns a tokenID at given index for owner
+    /// @param owner - address to fetch the NFT TokenID for
+    /// @param index - index of NFT in owned tokens array to fetch
+    /// @return Returns the TokenID of the NFT
+    function tokenOfOwnerByIndex(address owner, uint256 index)
+        external
+        view
+        returns (uint256);
+}
